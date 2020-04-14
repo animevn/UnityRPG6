86d8bfa954b6e9164bb2952ec510c84ed45ffc0c
animevn <haanhth21@gmail.com>
2020-04-14 21:04:15 +0700

    initial project

---
+++ Assets/Script/Controller/AIController.cs
@@ -0,0 +1,110 @@
+﻿﻿using System;
+using Script.Combat;
+using Script.Core;
+ using Script.Movement;
+ using UnityEngine;
+
+namespace Script.Controller
+{
+    // ReSharper disable once InconsistentNaming
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 10f;
+        [SerializeField] private float suspisionTime = 2f;
+        [SerializeField] private PatrolPath patrolPath;
+        [SerializeField] private float waypointTolerance = 1f;
+        [SerializeField] float waypointDwellTime = 3f;
+        
+        private Fighter fighter;
+        private Health health;
+        private Mover mover;
+        private GameObject player;
+        private Vector3 guardPosition;
+        private float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        private int currentWayPointIndex = 0;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            player = GameObject.FindWithTag("Player");
+            guardPosition = transform.position;
+        }
+
+        private bool InAttackRange()
+        {
+            return Vector3.Distance(player.transform.position, transform.position) < chaseDistance;
+        }
+
+        private void Update()
+        {
+            if (health.IsDead()) return;
+            if (InAttackRange() && fighter.CanAttack(player))
+            {
+                timeSinceLastSawPlayer = 0;
+                fighter.Attack(player);
+            }
+            else if (timeSinceLastSawPlayer < suspisionTime)
+            {
+                GetComponent<ActionScheduler>().CancelAction();
+            }
+            else
+            {
+                PatrolBehaviour();
+            }
+            UpdateTimer();
+        }
+
+        private void UpdateTimer()
+        {
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPostion = guardPosition;
+            if (patrolPath != null)
+            {
+                if (AtWayPoint())
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWayPoint();
+                }
+
+                nextPostion = GetCurrentWaypoint();
+            }
+            
+            if (timeSinceArrivedAtWaypoint > waypointDwellTime)
+            {
+                mover.StartToMoveTo(nextPostion);
+            }
+            
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWayPointIndex);
+        }
+
+        private void CycleWayPoint()
+        {
+            currentWayPointIndex = patrolPath.GetNextIndex(currentWayPointIndex);
+        }
+
+        private bool AtWayPoint()
+        {
+            var distance = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distance < waypointTolerance;
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+    }
+}
---
+++ Assets/Script/Movement/Mover.cs
@@ -0,0 +1,52 @@
+﻿using Script.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Script.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        // ReSharper disable once InconsistentNaming
+        private NavMeshAgent navMeshAgent;
+        private Health health;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            health = GetComponent<Health>();
+        }
+
+        private void Update()
+        {
+            if (health.IsDead()) navMeshAgent.enabled = false;
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            var velocity = navMeshAgent.velocity;
+            var localVelocity = transform.InverseTransformDirection(velocity);
+            var speed = localVelocity.z;
+            // ReSharper disable once Unity.PreferAddressByIdToGraphicsParams
+            GetComponent<Animator>().SetFloat("forward", speed);
+        }
+        
+        public void StartToMoveTo(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
