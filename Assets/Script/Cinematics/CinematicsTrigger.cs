549e6bf63d3f857b52e9b7ef484991eabbaf89f3
animevn <haanhth21@gmail.com>
2020-04-15 16:30:11 +0700

    trigger intro ok

---
+++ Assets/Script/Cinematics/CinematicsTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Script.Cinematics
+{
+    public class CinematicsTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            GetComponent<PlayableDirector>().Play();
+        }
+    }
+}
